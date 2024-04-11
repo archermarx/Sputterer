c02300635f621f781730010c56ddb8447595508f
Thomas Marks <marksta@umich.edu>
2024-04-11 14:15:43 -0400

    add multithreading to particle push

--- include/ParticleContainer.hpp
+++ include/ParticleContainer.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <thread>
 
 using std::string, std::vector;
 
@@ -14,24 +15,23 @@ template <typename T>
 class ParticleContainer {
     // Holds information for many particles of a specific species.
     // Species are differentiated by charge state and mass.
-    
+
     public:
         string name;        // name of particles
         T mass;             // mass in atomic mass units
         int charge;         // charge number
         int numParticles;   // number of particles in container
-        
 
         // Position in meters
         vector<T> position_x;
         vector<T> position_y;
         vector<T> position_z;
-    
+
         // Velocity in m/s
         vector<T> velocity_x;
         vector<T> velocity_y;
         vector<T> velocity_z;
-        
+
         // Particle weight (computational particles per real particle
         vector<T> weight;
 
@@ -39,7 +39,9 @@ class ParticleContainer {
         ParticleContainer(string name, T mass, int charge): name(name), mass(mass), charge(charge), numParticles(0) {};
 
         // push particles to next positions (for now just use forward Euler)
-        void push(T dt);
+        void push(T dt, int numThreads);
+
+        void pushSome(T dt, int begin, int end);
 
         // add particles to the container
         void addParticles(vector<T> x, vector<T> y, vector<T> z, vector<T> vx, vector<T> vy, vector<T> vz, vector<T> w);
@@ -48,9 +50,9 @@ class ParticleContainer {
 
 template <typename T>
 void ParticleContainer<T>::addParticles(vector<T> x, vector<T> y, vector<T> z, vector<T> ux, vector<T> uy, vector<T> uz, vector<T> w) {
-    
+
     auto N = std::min({x.size(), y.size(), z.size(), ux.size(), uy.size(), uz.size(), w.size()});
-    
+
     numParticles += N;
 
     position_x.insert(position_x.end(), x.begin(), x.begin() + N);
@@ -60,17 +62,39 @@ void ParticleContainer<T>::addParticles(vector<T> x, vector<T> y, vector<T> z, v
     velocity_x.insert(velocity_x.end(), ux.begin(), ux.begin() + N);
     velocity_y.insert(velocity_y.end(), uy.begin(), uy.begin() + N);
     velocity_z.insert(velocity_z.end(), uz.begin(), uz.begin() + N);
-   
+
     weight.insert(weight.end(), w.begin(), w.begin() + N);
 }
 
+
 template <typename T>
-void ParticleContainer<T>::push(T dt) {
-    for (int i = 0; i < numParticles; i++) {
+void ParticleContainer<T>::pushSome(T dt, int begin, int end) {
+    for (int i = begin; i < end; i++) {
         position_x[i] += velocity_x[i] * dt;
         position_y[i] += velocity_y[i] * dt;
         position_z[i] += velocity_z[i] * dt;
-    }   
+    }
+}
+
+template <typename T>
+void ParticleContainer<T>::push(T dt, int numThreads) {
+    std::thread threads[numThreads];
+
+    int chunk = numParticles / numThreads;
+    int remainder = numParticles - numThreads * chunk;
+
+    int start = 0;
+    for (int i = 0; i < numThreads; i++) {
+        int begin = start;
+        int count = chunk + static_cast<int>(i < remainder);
+        int end = begin + count;
+        start = end;
+        threads[i] = std::thread(&ParticleContainer<T>::pushSome, this, dt, begin, end);
+    }
+
+    for (int i = 0; i < numThreads; i++) {
+        threads[i].join();
+    }
 }
 
 template <typename T>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
 
 using std::vector, std::string;
 
@@ -12,17 +13,27 @@ int main() {
 
     ParticleContainer<float> pc("Carbon"s, 12.01f, 0);
 
-    vector<float> x = {0.0f};
-    vector<float> y = {0.0f};
-    vector<float> z = {0.0f};
-    vector<float> ux = {1.0f};
-    vector<float> uy = {0.5f};
-    vector<float> uz = {0.25f};
-    vector<float> w = {1.0f};
+    int numParticles = 1000000;
+    vector<float> x(numParticles, 0.0f);
+    vector<float> y(numParticles, 0.0f);
+    vector<float> z(numParticles, 0.0f);
+    vector<float> ux(numParticles, 1.0f);
+    vector<float> uy(numParticles, 0.5f);
+    vector<float> uz(numParticles, 0.25f);
+    vector<float> w(numParticles, 1.0f);
 
     pc.addParticles(x, y, z, ux, uy, uz, w);
-    std::cout << pc;
 
-    pc.push(1.0f);
-    std::cout << pc;
+    int numSteps = 1000;
+    float dt = 1.0f / numSteps;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < numSteps; i++) {
+        pc.push(dt, 4);
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
+
+    std::cout << duration << std::endl;
+    std::cout << "Average push duration: " << (float) duration / numSteps << " ms\n";
 }
