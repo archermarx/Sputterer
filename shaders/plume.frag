ee50624a4d4d563a1ee803ca691b63c6c7179098
Thomas Marks <marksta@umich.edu>
2024-05-01 10:06:24 -0400

    plume is translucent!

--- shaders/plume.frag
+++ shaders/plume.frag
@@ -3,5 +3,5 @@
 vec3 color = vec3(150.0, 229.0, 255.0)/255.0;
 
 void main() {
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, 0.2);
 }
--- src/Window.cpp
+++ src/Window.cpp
@@ -38,6 +38,8 @@ void Window::enable () {
   }
 
   glEnable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   open = true;
 
