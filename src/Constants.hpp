385c0e354c5f160f51b4b9cc5d64b2347a1c6fa0
Thomas Marks <marksta@umich.edu>
2024-05-03 08:51:18 -0400

    add basic species constants

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -5,6 +5,11 @@
 #ifndef SPUTTERER_CONSTANTS_HPP
 #define SPUTTERER_CONSTANTS_HPP
 
+struct Species {
+  double mass;
+  double atomic_number;
+};
+
 namespace constants {
   constexpr double eps_0 = 8.8541878128e-12;    // Vacuum permittivity [C^2 / eV / Å]
   constexpr double q_e = 1.602176634e-19;       // Fundamental charge [C]
@@ -12,7 +17,10 @@ namespace constants {
   constexpr double m_u = 1.66053906660e-27;     // One Dalton / atomic mass unit [kg]
   constexpr double k_b = 1.380649e-23;          // Boltzmann constant [J/K]
   constexpr double a_0 = 0.529177210903e-10;    // Bohr radius [m]
-  constexpr double pi = 3.14159265359;
+  constexpr double pi = 3.14159265359;          // pi
+
+  constexpr Species xenon{.mass = 131.293, .atomic_number = 54};
+  constexpr Species carbon{.mass = 12.011, .atomic_number = 6};
 }
 
 #endif //SPUTTERER_CONSTANTS_HPP
