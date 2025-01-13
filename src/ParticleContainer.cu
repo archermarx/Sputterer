be1f5f189b6bfd0a7a0a3e2d61cf259f33d51e84
Thomas Marks <marksta@umich.edu>
2025-01-13 15:06:21 -0500

    Removed commented-out string

--- src/ParticleContainer.cu
+++ src/ParticleContainer.cu
@@ -15,7 +15,6 @@
 // Setup RNG
 __global__ void k_setup_rng (curandState *rng, uint64_t seed) {
     unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    // curand_init(seed << 20, 0, 0, &rng[tid]);
     curand_init((seed << 20) + tid, 0, 0, &rng[tid]);
 }
 
